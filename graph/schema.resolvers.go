d38fb5bedb9f74de75c76bb3a7acc8c83df44e6d
hiroki <hiro@hirokinoMacBook-Pro.local>
2024-04-08 19:51:33 +0900

    [mod]自動生成されたファイルにresolverを実装

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -11,14 +11,25 @@ import (
 	"gqlgen-practice/graph/model"
 )
 
+// AddUser is the resolver for the addUser field.
+func (r *mutationResolver) AddUser(ctx context.Context, name string, email string) (*model.User, error) {
+	users[name] = email
+	return &model.User{Name: name, Email: email}, nil
+}
+
 // UpdateUser is the resolver for the updateUser field.
 func (r *mutationResolver) UpdateUser(ctx context.Context, name string, email string) (*model.User, error) {
-	panic(fmt.Errorf("not implemented: UpdateUser - updateUser"))
+	users[name] = email
+	return &model.User{Name: name, Email: email}, nil
 }
 
 // GetUser is the resolver for the getUser field.
 func (r *queryResolver) GetUser(ctx context.Context, name string) (*model.User, error) {
-	panic(fmt.Errorf("not implemented: GetUser - getUser"))
+	email, exists := users[name]
+	if !exists {
+		return nil, fmt.Errorf("user not found")
+	}
+	return &model.User{Name: name, Email: email}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -29,3 +40,11 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//   - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//     it when you're done.
+//   - You have helper methods in this file. Move them out to keep these resolver files clean.
+var users = map[string]string{}
